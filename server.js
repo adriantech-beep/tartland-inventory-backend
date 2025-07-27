02c026106024c8b5f56d6a1f0dfa887e5606b86b
adriantech-beep <Adhrianne29@gmail.com>
2025-07-27 16:47:22 +0800

    fixed corrs error

--- server.js
+++ server.js
@@ -49,10 +49,11 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps, curl, etc.)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) return callback(null, true);
-      return callback(new Error("Not allowed by CORS"));
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
     },
     credentials: true,
   })
